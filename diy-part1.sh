2849115931ef82006933f69ff44691bf4fba708f
ufdds025su <ufdds025su@yahoo.com.tw>
2022-05-03 18:35:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,9 +22,6 @@ sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-#sed -i '$a src-git dockerman https://github.com/lisaac/luci-app-dockerman' feeds.conf.default
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 #sed -i '$a src-git NueXiniPackages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #git clone https://github.com/kenzok8/small.git package/small
@@ -32,11 +29,11 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
 #git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
 #git clone https://github.com/NueXini/NueXini_Packages.git package/NueXini
-#rm -f /workdir/openwrt/build_dir/target-arm_cortex-a7+neon-vfpv4_musl_eabi/root-ipq40xx/sbin/mount.ntfs
-#Add The Theme：luci-theme-opentomcat
-#git clone https://github.com/Cosme927/luci-theme-opentomcat package/luci-theme-opentomcat
-# zerotier
-#sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.8.4/g' feeds/packages/net/zerotier/Makefile
-#sed -i 's/PKG_HASH:=.*/PKG_HASH:=2866a4ef9193cca0a9f0fe528a0dea00c13cb0fd714bf388a0300cb6f3639b3b/g' feeds/packages/net/zerotier/Makefile
-#rm -rf feeds/packages/net/zerotier
-#cp -rf $GITHUB_WORKSPACE/general/zerotier feeds/packages/net
+
+# Add luci-theme-argon
+# rm -rf lede/package/lean/luci-theme-argon
+# git clone https://github.com/jerrykuku/luci-theme-argon.git
+rm -rf package/lean/luci-theme-argon/
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
