21e0c4c880c8fcce0beddcf6a1aa79a901ab4517
ufdds025su <ufdds025su@yahoo.com.tw>
2022-05-14 09:05:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,15 +25,17 @@ sed -i 's/root::0:0:99999:7:::/root::0:0:99999:7:::/g' package/base-files/files/
 #sed -i '$a src-git NueXiniPackages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #git clone https://github.com/kenzok8/small.git package/small
+
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small-package' feeds.conf.default
+
 #git clone https://github.com/kiddin9/openwrt-packages.git package/kiddin9
 #git clone https://github.com/NueXini/NueXini_Packages.git package/NueXini
 
 # Add luci-theme-argon
-rm -rf lede/package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git
-rm -rf package/lean/luci-theme-argon/
+#rm -rf lede/package/lean/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git
+#rm -rf package/lean/luci-theme-argon/
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
